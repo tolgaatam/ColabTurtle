4949f2202fa14c19e488a53b34311572e27a81b1
Kamil Tolga Atam <atam@sabanciuniv.edu>
2018-11-07 12:36:17 +0300

    turn to non-class implementation2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ColabTurtle',
-    version='1.0.0',
+    version='2.0.0',
     packages=['ColabTurtle'],
     url='https://github.com/tolgaatam/ColabTurtle',
     license='MIT',
