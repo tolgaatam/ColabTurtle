4d8c1fac07d909192b5f4d5fc34c30bd8c74cf1f
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-03-24 16:30:18 -0400

    Update Turtle.py

--- ColabTurtle/Turtle.py
+++ ColabTurtle/Turtle.py
@@ -94,17 +94,17 @@ def initializeTurtle(initial_speed=DEFAULT_SPEED, initial_window_size=DEFAULT_WI
     window_size = initial_window_size
 
     is_turtle_visible = DEFAULT_TURTLE_VISIBILITY
-    if initial_color.lower()=="white":
-        pen_color = "black"
-    else:
-        pen_color = DEFAULT_PEN_COLOR
+    #if initial_color.lower()=="white":
+    #    pen_color = "black"
+    #else:
+    #    pen_color = DEFAULT_PEN_COLOR
     turtle_pos = (window_size[0] // 2, window_size[1] // 2)
     turtle_degree = DEFAULT_TURTLE_DEGREE
     #background_color = DEFAULT_BACKGROUND_COLOR
-    background_color = initial_color
+    #background_color = initial_color
     is_pen_down = DEFAULT_IS_PEN_DOWN
     svg_lines_string = DEFAULT_SVG_LINES_STRING
-    pen_width = DEFAULT_PEN_WIDTH
+    #pen_width = DEFAULT_PEN_WIDTH
     #turtle_shape = DEFAULT_TURTLE_SHAPE
     turtle_shape = initial_turtle.lower()
 
