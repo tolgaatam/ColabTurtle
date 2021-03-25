15a3018d3d24acc9e6f357524d42feb802ff322a
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-03-24 21:36:28 -0400

    Update Turtle.py

--- ColabTurtle/Turtle.py
+++ ColabTurtle/Turtle.py
@@ -129,7 +129,7 @@ def _generateTurtleSvgDrawing():
         degrees += 90
         template = TURTLE_TURTLE_SVG_TEMPLATE
     elif turtle_shape == 'arrow':
-        turtle_x += 10
+        #turtle_x += 10
         #turtle_y -= 10
         degrees -= 90
         template = TURTLE_ARROW_SVG_TEMPLATE
