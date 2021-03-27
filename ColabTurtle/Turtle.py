4a30c4224388eef128af4a3e78d41f3e2b2aaa71
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-03-27 11:45:20 -0400

    Update Turtle.py

--- ColabTurtle/Turtle.py
+++ ColabTurtle/Turtle.py
@@ -89,23 +89,16 @@ def initializeTurtle(initial_speed=DEFAULT_SPEED, initial_window_size=DEFAULT_WI
     if not (isinstance(initial_window_size, tuple) and len(initial_window_size) == 2 and isinstance(
             initial_window_size[0], int) and isinstance(initial_window_size[1], int)):
         raise ValueError('window_size must be a tuple of 2 integers')
-
     window_size = initial_window_size
 
     is_turtle_visible = DEFAULT_TURTLE_VISIBILITY
-    #if initial_color.lower()=="white":
-    #    pen_color = "black"
-    #else:
-    #    pen_color = DEFAULT_PEN_COLOR
     turtle_pos = (window_size[0] // 2, window_size[1] // 2)
     turtle_degree = DEFAULT_TURTLE_DEGREE
     background_color = DEFAULT_BACKGROUND_COLOR
-    background_color = initial_color
     is_pen_down = DEFAULT_IS_PEN_DOWN
     svg_lines_string = DEFAULT_SVG_LINES_STRING
     pen_width = DEFAULT_PEN_WIDTH
     turtle_shape = DEFAULT_TURTLE_SHAPE
-    #turtle_shape = initial_turtle.lower()
 
     drawing_window = display(HTML(_generateSvgDrawing()), display_id=True)
 
