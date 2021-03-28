0a1e9bcd196abc2cae5988170cb57c41eea18f88
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-03-28 17:55:26 -0400

    Update Turtle.py

--- ColabTurtle/Turtle.py
+++ ColabTurtle/Turtle.py
@@ -574,7 +574,7 @@ def showSVG(show_turtle=False):
     header = ("""<svg width="{w}" height="{h}" viewBox="0 0 {w} {h}" xmlns="http://www.w3.org/2000/svg">\n""").format(w=window_size[0],
                                                                                                                     h=window_size[1],
                                                                                                                     kolor=background_color) 
-    header += <rect width="100%" height="100%" style="fill:{kolor}" />\n""")
+    header += """<rect width="100%" height="100%" style="fill:{kolor}" />\n"""
     image = svg_lines_string.replace(">",">\n")
     if show_turtle:
         turtle_svg = _generateTurtleSvgDrawing() + " \n"
