e657c8683304026657534919eac5eabd59b19a67
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-03-30 16:54:29 -0400

    Update Turtle.py

--- ColabTurtle/Turtle.py
+++ ColabTurtle/Turtle.py
@@ -121,7 +121,7 @@ def initializeTurtle(initial_speed=DEFAULT_SPEED, initial_window_size=DEFAULT_WI
 
     is_turtle_visible = DEFAULT_TURTLE_VISIBILITY
     turtle_pos = (window_size[0] // 2, window_size[1] // 2)
-    turtle_degree = DEFAULT_TURTLE_DEGREE if (angle_mode == 'standard') else (360 - DEFAULT_TURTLE_DEGREE)
+    turtle_degree = DEFAULT_TURTLE_DEGREE if (angle_mode == 'standard') else (270 - DEFAULT_TURTLE_DEGREE)
     background_color = DEFAULT_BACKGROUND_COLOR
     is_pen_down = DEFAULT_IS_PEN_DOWN
     svg_lines_string = DEFAULT_SVG_LINES_STRING
@@ -352,7 +352,7 @@ def getheading():
     if angle_mode == 'standard':
         return (360 - turtle_degree) % 360
     else:
-        return turtle_degree
+        return turtle_degree % 360
 
 heading = getheading # alias
 
