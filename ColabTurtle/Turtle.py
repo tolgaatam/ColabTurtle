109a6fe9178b7ce19d24f2c84c0e2971b660e460
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-04-07 21:39:31 -0400

    Update Turtle.py

--- ColabTurtle/Turtle.py
+++ ColabTurtle/Turtle.py
@@ -264,7 +264,7 @@ def _arctoNewPosition(r,new_pos):
     
     start_pos = turtle_pos
     if is_pen_down:
-        svg_lines_string += """<path d="M {x1} {y1} A {rx} {ry} 0 0 0 {x2} {y2}" stroke-linecap="round" fill="transparent" style="stroke:{pen_color};stroke-width:{pen_width}"/>""".format(
+        svg_lines_string += """<path d="M {x1} {y1} A {rx} {ry} 0 0 1 {x2} {y2}" stroke-linecap="round" fill="transparent" style="stroke:{pen_color};stroke-width:{pen_width}"/>""".format(
             x1=start_pos[0], y1=start_pos[1],rx = r, ry = r, x2=new_pos[0], y2=new_pos[1], pen_color=pen_color, pen_width=pen_width)    
     if is_filling:
         svg_fill_string += """ A {rx} {ry} 0 0 0 {x2} {y2} """.format(rx=r,ry=r,x2=new_pos[0],y2=new_pos[1])
@@ -305,8 +305,8 @@ def arc(radius, degrees):
     gamma = theta+alpha-math.radians(90)
     
     circle_center = (turtle_pos[0] + radius * xscale * math.sin(alpha), turtle_pos[1] - radius * abs(yscale) * math.cos(alpha))
-    ending_point = (circle_center[0] + radius*math.cos(gamma) ,circle_center[1] + radius*math.sin(gamma))
-    print(circle_center)
+    ending_point = (circle_center[0] + radius*math.cos(gamma) , circle_center[1] + radius*math.sin(gamma))
+   
     _arctoNewPosition(radius,ending_point)
     
     turtle_degree = (turtle_degree - degrees) % 360
