1c3458a0ed594d368f8f638f43727a39174f896a
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-04-10 10:52:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='ColabTurtle',
-    version='2.0.0',
-    packages=['ColabTurtle'],
-    url='https://github.com/tolgaatam/ColabTurtle',
+    version='1.0.0',
+    packages=['ColabTurtleB'],
+    url='https://github.com/mathriddle/ColabTurtleB',
     license='MIT',
-    author='Tolga Atam',
-    author_email='atam@sabanciuniv.edu',
+    author='Larry Riddle',
+    author_email='lriddle@agnesscott.edu',
     description='An HTML based Turtle implementation, in order to work in Google Colab'
 )
