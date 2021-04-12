8f05323234023bf245824ff8c3fd12a3faa43dde
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-04-12 12:46:43 -0400

    Update Turtle.py

--- ColabTurtle/Turtle.py
+++ ColabTurtle/Turtle.py
@@ -370,7 +370,7 @@ def dot(size = None, *color):
     print(color)
     if not color:
         if isinstance(size, (str, tuple)):
-            color = _processColor(color)
+            color = _processColor(color[0])
             size = pen_width + max(pen_width,4)
         else:
             color = pen_color
@@ -379,7 +379,7 @@ def dot(size = None, *color):
     else:
         if size is None:
             size = pen_width + max(pen_width,4)
-        color = _processColor(color)
+        color = _processColor(color[0])
     svg_lines_string += """<circle cx="{cx}" cy="{cy}" r="{radius}" stroke="" fill="{kolor}" />""".format(radius=size,
                                                                                                       cx=turtle_pos[0],
                                                                                                       cy=turtle_pos[1],
